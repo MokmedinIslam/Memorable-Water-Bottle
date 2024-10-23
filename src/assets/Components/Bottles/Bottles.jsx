c97e726d953b72f7873d2954e889964b4971d154
Mokmedin <mokmedini@gmail.com>
2024-10-23 17:15:41 +0600

    cart

--- src/assets/Components/Bottles/Bottles.jsx
+++ src/assets/Components/Bottles/Bottles.jsx
@@ -6,18 +6,30 @@ import './bottles.css'
 const Bottles = () => {
 
     const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect( () => {
         fetch('bottles.json')
         .then(res => res.json())
         .then(data => setBottles(data))
     }, []);
+
+    const handleAddToCart = bottle => {
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+    }
+
     return (
         <div>
-            <h2>Bottles Here: {bottles.length}</h2>
+            <h2>Bottles Available: {bottles.length}</h2>
+            <h3>Cart: {cart.length}</h3>
             <div className="bottle-container">
             {
-                bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+                bottles.map(bottle => <Bottle 
+                    key={bottle.id} 
+                    bottle={bottle}
+                    handleAddToCart={handleAddToCart}
+                    ></Bottle>)
             }
             </div>
         </div>
